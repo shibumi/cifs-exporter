3648acb4eab1f7ebdf9c0550f232d7b1b1b696ab
Christian Rebischke <chris@shibumi.dev>
2020-03-22 02:10:27 +0100

    implement first idea for parsing SMB blocks

--- cifs/cifs.go
+++ cifs/cifs.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 )
 
 type ClientStats struct {
-	Header   Header
-	SMB1List []*SMB1
-	SMB2List []*SMB2
+	Header      Header
+	SMB1List    []*SMB1
+	SMB2List    []*SMB2
+	CacheId     uint64
+	CacheServer string
+	CacheShare  string
 }
 
 type SMB1 struct {
@@ -98,6 +102,8 @@ type Header struct {
 	AtOnce          uint64
 }
 
+var re = regexp.MustCompile(`(?m)(?:(?:(?P<SMBID>\d+)\) \\\\(?P<Server>[A-Za-z1-9-.]+)(?P<Share>.+)\nSMBs:\s+(?P<SMB>\d+) Oplocks breaks:\s+(?P<OpLocks>\d+)\nReads:\s+(?P<Reads>\d+) Bytes:\s+(?P<ReadsBytes>\d+)\nWrites:\s+(?P<Writes>\d+) Bytes:\s+(?P<WritesBytes>\d+)\nFlushes:\s+(?P<Flushes>\d+)\nLocks:\s+(?P<Locks>\d+) HardLinks:\s+(?P<Hardlinks>\d+) Symlinks:\s+(?P<Symlinks>\d+)\nOpens:\s+(?P<Opens>\d+) Closes:\s+(?P<Closes>\d+) Deletes:\s+(?P<Deletes>\d+)\nPosix Opens:\s+(?P<PosixOpens>\d+) Posix Mkdirs:\s+(?P<PosixMkdirs>\d+)\nMkdirs:\s+(?P<Mkdirs>\d+) Rmdirs:\s+(?P<Rmdirs>\d+)\nRenames:\s+(?P<Renames>\d+) T2 Renames\s+(?P<T2Renames>\d+)\nFindFirst:\s+(?P<FindFirst>\d+) FNext\s+(?P<FNext>\d+) FClose\s+(?P<FClose>\d+)|(?P<SMB3ID>\d+)\) \\\\(?P<SMB3Server>[A-Za-z1-9-.]+)(?P<SMB3Share>.+)\nSMBs:\s+(?P<SMB3>\d+)\nNegotiates:\s+(?P<NegotiatesSent>\d+) sent\s+(?P<NegotiatesFailed>\d+) failed\nSessionSetups:\s+(?P<SessionSetupsSent>\d+) sent\s+(?P<SessionSetupsFailed>\d+) failed\nLogoffs:\s+(?P<LogoffsSent>\d+) sent\s+(?P<LogoffsFailed>\d+) failed\nTreeConnects:\s+(?P<TreeConnectsSent>\d+) sent\s+(?P<TreeConnectsFailed>\d+) failed\nTreeDisconnects:\s+(?P<TreeDisconnectsSent>\d+) sent\s+(?P<TreeDisconnectsFailed>\d+) failed\nCreates:\s+(?P<CreatesSent>\d+) sent\s+(?P<CreatesFailed>\d+) failed\nCloses:\s+(?P<ClosesSent>\d+) sent\s+(?P<ClosesFailed>\d+) failed\nFlushes:\s+(?P<FlushesSent>\d+) sent\s+(?P<FlushesFailed>\d+) failed\nReads:\s+(?P<ReadsSent>\d+) sent\s+(?P<ReadsFailed>\d+) failed\nWrites:\s+(?P<WritesSent>\d+) sent\s+(?P<WritesFailed>\d+) failed\nLocks:\s+(?P<LocksSent>\d+) sent\s+(?P<LocksFailed>\d+) failed\nIOCTLs:\s+(?P<IOCTLsSent>\d+) sent\s+(?P<IOCTLsFailed>\d+) failed\nCancels:\s+(?P<CancelsSent>\d+) sent\s+(?P<CancelsFailed>\d+) failed\nEchos:\s+(?P<EchosSent>\d+) sent\s+(?P<EchosFailed>\d+) failed\nQueryDirectories:\s+(?P<QueryDirectoriesSent>\d+) sent\s+(?P<QueryDirectoriesFailed>\d+) failed\nChangeNotifies:\s+(?P<ChangeNotifiesSent>\d+) sent\s+(?P<ChangeNotifiesFailed>\d+) failed\nQueryInfos:\s+(?P<QueryInfosSent>\d+) sent\s+(?P<QueryInfosFailed>\d+) failed\nSetInfos:\s+(?P<SetInfosSent>\d+) sent\s+(?P<SetInfosFailed>\d+) failed\nOplockBreaks:\s+(?P<OpLockBreaksSent>\d+) sent\s+(?P<OpLockBreaksFailed>\d+) failed)+)`)
+
 func NewClientStats() (*ClientStats, error) {
 	f, err := os.Open("cifs/stats_example.txt")
 	if err != nil {
@@ -131,6 +137,15 @@ func (stats *ClientStats) parseHeader(line string) {
 	}
 }
 
+func (stats *ClientStats) parseSMBBlocks(file string) ([][]string, []string) {
+	matches := re.FindAllStringSubmatch(file, -1)
+	expNames := re.SubexpNames()
+	for i := 1; i < len(expNames); i++ {
+		fmt.Println(expNames[i])
+	}
+	return matches, expNames
+}
+
 func ParseClientStats(r io.Reader) (*ClientStats, error) {
 	stats := &ClientStats{}
 	scanner := bufio.NewScanner(r)
@@ -144,10 +159,12 @@ func ParseClientStats(r io.Reader) (*ClientStats, error) {
 		stats.parseHeader(line)
 		headerLen--
 	}
-	// parse SMB blocks
+	// construct SMB block file
+	var file string
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		file += line + "\n"
 	}
+	stats.parseSMBBlocks(file)
 	return stats, nil
 }
